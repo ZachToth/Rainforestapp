41d0d8d474359350625f58f0b8fa7625485f06ec
Zachary Toth <zachtoth@gmail.com>
2015-02-10 14:34:00 -0500

    Added users

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ActiveRecord::Base
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
 
-  def cents_to_dollars
+  def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
-    sprintf("%.2", price_in_dollars)
+    sprintf("%.2f", price_in_dollars)
   end
 end
